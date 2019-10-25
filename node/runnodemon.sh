05b6cc764572bffc6d94d6189cc920c27f4f4def
shivut <40708954+shivut@users.noreply.github.com>
2019-10-25 16:28:56 +0530

    provided the correct log path

--- node/runnodemon.sh
+++ node/runnodemon.sh
@@ -3,7 +3,7 @@
 mkdir -p /mnt/pssd/node/platform/yedvanshitechapis/scratch
 while :
 do
-    /usr/bin/nodemon -I --exitcrash /mnt/pssd/node/platform/yedvanshitechapis/node/gobikesapi.js 2>&1 > /mnt/pssd/node/platform/yedvanshitechapis/scratch/gobikesapi.log
+    /usr/bin/nodemon -I --exitcrash /mnt/pssd/node/platform/yedvanshitechapis/node/yedvanshitech.js 2>&1 > /mnt/pssd/node/platform/yedvanshitechapis/scratch/yedvanshitech.log
     sleep 5
 done
 
